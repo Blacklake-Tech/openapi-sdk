d29fa5025d563740935853b3f4b005fbc5d97dc4
ZHANG XIOAYU <zxy@blacklake.cn>
2024-02-02 16:25:02 +0800

    HHZZ3-74491 quality 拆module

--- oapi-sdk/src/main/java/tech/blacklake/dev/openapi/sdk/BlacklakeSdkClient.java
+++ oapi-sdk/src/main/java/tech/blacklake/dev/openapi/sdk/BlacklakeSdkClient.java
@@ -4,12 +4,26 @@ import tech.blacklake.dev.holyfile.open.File4CustomObjectOpenApi;
 import tech.blacklake.dev.openapi.sdk.config.Config;
 import tech.blacklake.dev.openapi.sdk.constants.enums.AppTypeEnum;
 import tech.blacklake.dev.openapi.sdk.constants.enums.BaseUrlEnum;
+import tech.blacklake.dev.quality.api.open.def.checkitem.QcCheckItemCategoryOpenApi;
+import tech.blacklake.dev.quality.api.open.def.checkitem.QcCheckItemOpenApi;
+import tech.blacklake.dev.quality.api.open.def.qcconfig.QcConfigOpenApi;
+import tech.blacklake.dev.quality.api.open.def.qcconfig.QcDefectRankOpenApi;
+import tech.blacklake.dev.quality.api.open.def.qcconfig.QcDefectReasonOpenApi;
+import tech.blacklake.dev.quality.api.open.domain.QcMaterialOpenApi;
+import tech.blacklake.dev.quality.api.open.domain.QcTaskOpenApi;
 
 public class BlacklakeSdkClient {
 
     private Config config;
 
     public File4CustomObjectOpenApi file4CustomObjectOpenApi;
+    public QcCheckItemCategoryOpenApi qcCheckItemCategoryOpenApi;
+    public QcCheckItemOpenApi qcCheckItemOpenApi;
+    public QcConfigOpenApi qcConfigOpenApi;
+    public QcDefectRankOpenApi qcDefectRankOpenApi;
+    public QcDefectReasonOpenApi qcDefectReasonOpenApi;
+    public QcMaterialOpenApi qcMaterialOpenApi;
+    public QcTaskOpenApi qcTaskOpenApi;
 
     public static Builder newBuilder(String appKey, String appSecret) {
         return new Builder(appKey, appSecret);
--- oapi-sdk/src/main/java/tech/blacklake/dev/openapi/sdk/config/OpenapiSdkAutoConfig.java
+++ oapi-sdk/src/main/java/tech/blacklake/dev/openapi/sdk/config/OpenapiSdkAutoConfig.java
@@ -1,8 +1,5 @@
 package tech.blacklake.dev.openapi.sdk.config;
 
-import static tech.blacklake.dev.openapi.sdk.constants.Constants.HOLYFILE_PREFIX;
-import static tech.blacklake.dev.openapi.sdk.constants.Constants.ROUTE_URL;
-
 import feign.Retryer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -19,6 +16,15 @@ import tech.blacklake.dev.openapi.sdk.client.interceptor.ClientReqInterceptor;
 import tech.blacklake.dev.openapi.sdk.constants.enums.AppTypeEnum;
 import tech.blacklake.dev.openapi.sdk.event.ServletAdapter;
 import tech.blacklake.dev.openapi.sdk.token.TokenManager;
+import tech.blacklake.dev.quality.api.open.def.checkitem.QcCheckItemCategoryOpenApi;
+import tech.blacklake.dev.quality.api.open.def.checkitem.QcCheckItemOpenApi;
+import tech.blacklake.dev.quality.api.open.def.qcconfig.QcConfigOpenApi;
+import tech.blacklake.dev.quality.api.open.def.qcconfig.QcDefectRankOpenApi;
+import tech.blacklake.dev.quality.api.open.def.qcconfig.QcDefectReasonOpenApi;
+import tech.blacklake.dev.quality.api.open.domain.QcMaterialOpenApi;
+import tech.blacklake.dev.quality.api.open.domain.QcTaskOpenApi;
+
+import static tech.blacklake.dev.openapi.sdk.constants.Constants.*;
 
 @Configuration
 @EnableConfigurationProperties(Config.class)
@@ -50,6 +56,16 @@ public class OpenapiSdkAutoConfig {
             .logReqAtDebug(true)
             .build();
         blacklakeSdkClient.file4CustomObjectOpenApi = file4CustomObjectOpenApi(config);
+        /****quality begin********************/
+        blacklakeSdkClient.qcCheckItemCategoryOpenApi = qcCheckItemCategoryOpenApi(config);
+        blacklakeSdkClient.qcCheckItemOpenApi = qcCheckItemOpenApi(config);
+        blacklakeSdkClient.qcConfigOpenApi = qcConfigOpenApi(config);
+        blacklakeSdkClient.qcDefectRankOpenApi = qcDefectRankOpenApi(config);
+        blacklakeSdkClient.qcDefectReasonOpenApi = qcDefectReasonOpenApi(config);
+        blacklakeSdkClient.qcMaterialOpenApi = qcMaterialOpenApi(config);
+        blacklakeSdkClient.qcTaskOpenApi = qcTaskOpenApi(config);
+        /****quality end********************/
+
         return blacklakeSdkClient;
     }
 
@@ -85,6 +101,54 @@ public class OpenapiSdkAutoConfig {
         FeignClientBuilder feignClientBuilder = new FeignClientBuilder(this.applicationContext);
         return feignClientBuilder.forType(File4CustomObjectOpenApi.class, "file4CustomObjectOpenApi")
             .url(config.getBaseUrl() + ROUTE_URL + HOLYFILE_PREFIX)
+            .build();
+    }
+
+    private QcCheckItemCategoryOpenApi qcCheckItemCategoryOpenApi(Config config) {
+        FeignClientBuilder feignClientBuilder = new FeignClientBuilder(this.applicationContext);
+        return feignClientBuilder.forType(QcCheckItemCategoryOpenApi.class, "qcCheckItemCategoryOpenApi")
+            .url(config.getBaseUrl() + ROUTE_URL + QUALITY_PREFIX)
+            .build();
+    }
+    private QcCheckItemOpenApi qcCheckItemOpenApi(Config config) {
+        FeignClientBuilder feignClientBuilder = new FeignClientBuilder(this.applicationContext);
+        return feignClientBuilder.forType(QcCheckItemOpenApi.class, "qcCheckItemOpenApi")
+            .url(config.getBaseUrl() + ROUTE_URL + QUALITY_PREFIX)
+            .build();
+    }
+
+    private QcConfigOpenApi qcConfigOpenApi(Config config) {
+        FeignClientBuilder feignClientBuilder = new FeignClientBuilder(this.applicationContext);
+        return feignClientBuilder.forType(QcConfigOpenApi.class, "qcConfigOpenApi")
+            .url(config.getBaseUrl() + ROUTE_URL + QUALITY_PREFIX)
+            .build();
+    }
+
+    private QcDefectRankOpenApi qcDefectRankOpenApi(Config config) {
+        FeignClientBuilder feignClientBuilder = new FeignClientBuilder(this.applicationContext);
+        return feignClientBuilder.forType(QcDefectRankOpenApi.class, "qcDefectRankOpenApi")
+            .url(config.getBaseUrl() + ROUTE_URL + QUALITY_PREFIX)
+            .build();
+    }
+
+    private QcDefectReasonOpenApi qcDefectReasonOpenApi(Config config) {
+        FeignClientBuilder feignClientBuilder = new FeignClientBuilder(this.applicationContext);
+        return feignClientBuilder.forType(QcDefectReasonOpenApi.class, "qcDefectReasonOpenApi")
+            .url(config.getBaseUrl() + ROUTE_URL + QUALITY_PREFIX)
+            .build();
+    }
+
+    private QcMaterialOpenApi qcMaterialOpenApi(Config config) {
+        FeignClientBuilder feignClientBuilder = new FeignClientBuilder(this.applicationContext);
+        return feignClientBuilder.forType(QcMaterialOpenApi.class, "qcMaterialOpenApi")
+            .url(config.getBaseUrl() + ROUTE_URL + QUALITY_PREFIX)
+            .build();
+    }
+
+    private QcTaskOpenApi qcTaskOpenApi(Config config) {
+        FeignClientBuilder feignClientBuilder = new FeignClientBuilder(this.applicationContext);
+        return feignClientBuilder.forType(QcTaskOpenApi.class, "qcTaskOpenApi")
+            .url(config.getBaseUrl() + ROUTE_URL + QUALITY_PREFIX)
             .build();
     }
 }
