c19d857ae9b4a5bfce1d23bf7e9fa26d0744fffd
limenglong <limenglong@blacklake.cn>
2024-05-08 19:54:37 +0800

    1

--- oapi-sdk/src/main/java/tech/blacklake/dev/openapi/sdk/BlacklakeSdkClient.java
+++ oapi-sdk/src/main/java/tech/blacklake/dev/openapi/sdk/BlacklakeSdkClient.java
@@ -5,6 +5,8 @@ import tech.blacklake.dev.openapi.sdk.config.Config;
 import tech.blacklake.dev.openapi.sdk.constants.enums.AppTypeEnum;
 import tech.blacklake.dev.openapi.sdk.constants.enums.BaseUrlEnum;
 import tech.blacklake.dev.order.open.OpenCustomerApi;
+import tech.blacklake.dev.order.open.OpenCustomerV2Api;
+import tech.blacklake.dev.order.open.*;
 
 public class BlacklakeSdkClient {
 
@@ -12,6 +14,42 @@ public class BlacklakeSdkClient {
 
     public File4CustomObjectOpenApi file4CustomObjectOpenApi;
     public OpenCustomerApi openCustomerApi;
+    public OpenCustomerV2Api openCustomerV2Api;
+    public OpenDeliveryAppointmentApi openDeliveryAppointmentApi;
+    public OpenDeliveryAppointmentV2Api openDeliveryAppointmentV2Api;
+    public OpenDeliveryScheduleApi openDeliveryScheduleApi;
+    public OpenDeliveryScheduleNoteApi openDeliveryScheduleNoteApi;
+    public OpenDeliveryScheduleNoteV2Api openDeliveryScheduleNoteV2Api;
+    public OpenDeliveryScheduleV2Api openDeliveryScheduleV2Api;
+    public OpenOrderChangeNoteApi openOrderChangeNoteApi;
+    public OpenOrderChangeNoteV2Api openOrderChangeNoteV2Api;
+    public OpenOutsourceSendMaterialApi openOutsourceSendMaterialApi;
+    public OpenOutsourceSendMaterialV2APi openOutsourceSendMaterialV2APi;
+    public OpenPlanFeedbackApi openPlanFeedbackApi;
+    public OpenPurchaseOrderApi openPurchaseOrderApi;
+    public OpenPurchaseOrderV2Api openPurchaseOrderV2Api;
+    public OpenPurchasePlanApi openPurchasePlanApi;
+    public OpenPurchaseRequisitionsApi openPurchaseRequisitionsApi;
+    public OpenPurchaseRequisitionsV2Api openPurchaseRequisitionsV2Api;
+    public OpenPurchaseReturnOrderApi openPurchaseReturnOrderApi;
+    public OpenPurchaseReturnOrderV2Api openPurchaseReturnOrderV2Api;
+    public OpenReceiveNoteApi openReceiveNoteApi;
+    public OpenReceiveNoteV2Api openReceiveNoteV2Api;
+    public OpenReceivingRecordApi openReceivingRecordApi;
+    public OpenSalesOrderApi openSalesOrderApi;
+    public OpenSalesOrderReturnApi openSalesOrderReturnApi;
+    public OpenSalesOrderReturnV2Api openSalesOrderReturnV2Api;
+    public OpenSalesOrderV2Api openSalesOrderV2Api;
+    public OpenShipmentNoteApi openShipmentNoteApi;
+    public OpenShipmentNoteV2Api openShipmentNoteV2Api;
+    public OpenShipmentRecordApi openShipmentRecordApi;
+    public OpenShipmentRecordV2Api openShipmentRecordV2Api;
+    public OpenSourceListApi openSourceListApi;
+    public OpenSourceListV2Api openSourceListV2Api;
+    public OpenSupplierApi openSupplierApi;
+    public OpenSupplierV2Api openSupplierV2Api;
+    public OpenWaitingReceivingListApi openWaitingReceivingListApi;
+    public OpenWaitingReceivingListV2Api openWaitingReceivingListV2Api;
 
     public static Builder newBuilder(String appKey, String appSecret) {
         return new Builder(appKey, appSecret);
--- oapi-sdk/src/main/java/tech/blacklake/dev/openapi/sdk/config/OpenapiSdkAutoConfig.java
+++ oapi-sdk/src/main/java/tech/blacklake/dev/openapi/sdk/config/OpenapiSdkAutoConfig.java
@@ -19,7 +19,7 @@ import tech.blacklake.dev.openapi.sdk.client.interceptor.ClientReqInterceptor;
 import tech.blacklake.dev.openapi.sdk.constants.enums.AppTypeEnum;
 import tech.blacklake.dev.openapi.sdk.event.ServletAdapter;
 import tech.blacklake.dev.openapi.sdk.token.TokenManager;
-import tech.blacklake.dev.order.open.OpenCustomerApi;
+import tech.blacklake.dev.order.open.*;
 
 @Configuration
 @EnableConfigurationProperties(Config.class)
@@ -52,7 +52,187 @@ public class OpenapiSdkAutoConfig {
             .build();
         blacklakeSdkClient.file4CustomObjectOpenApi = file4CustomObjectOpenApi(config);
         blacklakeSdkClient.openCustomerApi = customerOpenApi(config);
+        blacklakeSdkClient.openCustomerV2Api = openCustomerV2Api(config);
+        blacklakeSdkClient.openDeliveryAppointmentApi = openDeliveryAppointmentApi(config);
+        blacklakeSdkClient.openDeliveryAppointmentV2Api = openDeliveryAppointmentV2Api(config);
+        blacklakeSdkClient.openDeliveryScheduleApi = openDeliveryScheduleApi(config);
+        blacklakeSdkClient.openDeliveryScheduleNoteApi = openDeliveryScheduleNoteApi(config);
+        blacklakeSdkClient.openDeliveryScheduleNoteV2Api = openDeliveryScheduleNoteV2Api(config);
+        blacklakeSdkClient.openDeliveryScheduleV2Api = openDeliveryScheduleV2Api(config);
+        blacklakeSdkClient.openOrderChangeNoteApi = openOrderChangeNoteApi(config);
+        blacklakeSdkClient.openOrderChangeNoteV2Api = openOrderChangeNoteV2Api(config);
+        blacklakeSdkClient.openOutsourceSendMaterialApi = openOutsourceSendMaterialApi(config);
+        blacklakeSdkClient.openOutsourceSendMaterialV2APi = openOutsourceSendMaterialV2APi(config);
+        blacklakeSdkClient.openPlanFeedbackApi = openPlanFeedbackApi(config);
+        blacklakeSdkClient.openPurchaseOrderApi = openPurchaseOrderApi(config);
+        blacklakeSdkClient.openPurchaseOrderV2Api = openPurchaseOrderV2Api(config);
+        blacklakeSdkClient.openPurchasePlanApi = openPurchasePlanApi(config);
+        blacklakeSdkClient.openPurchaseRequisitionsApi = openPurchaseRequisitionsApi(config);
+        blacklakeSdkClient.openPurchaseRequisitionsV2Api = openPurchaseRequisitionsV2Api(config);
+        blacklakeSdkClient.openPurchaseReturnOrderApi = openPurchaseReturnOrderApi(config);
+        blacklakeSdkClient.openPurchaseReturnOrderV2Api = openPurchaseReturnOrderV2Api(config);
+        blacklakeSdkClient.openReceiveNoteApi = openReceiveNoteApi(config);
+        blacklakeSdkClient.openReceiveNoteV2Api = openReceiveNoteV2Api(config);
+        blacklakeSdkClient.openReceivingRecordApi = openReceivingRecordApi(config);
+        blacklakeSdkClient.openSalesOrderApi = openSalesOrderApi(config);
+        blacklakeSdkClient.openSalesOrderReturnApi = openSalesOrderReturnApi(config);
+        blacklakeSdkClient.openSalesOrderReturnV2Api = openSalesOrderReturnV2Api(config);
+        blacklakeSdkClient.openSalesOrderV2Api = openSalesOrderV2Api(config);
+        blacklakeSdkClient.openShipmentNoteApi = openShipmentNoteApi(config);
+        blacklakeSdkClient.openShipmentNoteV2Api = openShipmentNoteV2Api(config);
+        blacklakeSdkClient.openShipmentRecordApi = openShipmentRecordApi(config);
+        blacklakeSdkClient.openShipmentRecordV2Api = openShipmentRecordV2Api(config);
+        blacklakeSdkClient.openSourceListApi = openSourceListApi(config);
+        blacklakeSdkClient.openSourceListV2Api = openSourceListV2Api(config);
+        blacklakeSdkClient.openSupplierApi = openSupplierApi(config);
+        blacklakeSdkClient.openSupplierV2Api = openSupplierV2Api(config);
+        blacklakeSdkClient.openWaitingReceivingListApi = openWaitingReceivingListApi(config);
+        blacklakeSdkClient.openWaitingReceivingListV2Api = openWaitingReceivingListV2Api(config);
         return blacklakeSdkClient;
+    }
+
+    private OpenWaitingReceivingListV2Api openWaitingReceivingListV2Api(Config config) {
+        return null;
+    }
+
+    private OpenWaitingReceivingListApi openWaitingReceivingListApi(Config config) {
+        return null;
+    }
+
+    private OpenSupplierV2Api openSupplierV2Api(Config config) {
+        return null;
+    }
+
+    private OpenSupplierApi openSupplierApi(Config config) {
+        return null;
+    }
+
+    private OpenSourceListV2Api openSourceListV2Api(Config config) {
+        return null;
+    }
+
+    private OpenSourceListApi openSourceListApi(Config config) {
+        return null;
+    }
+
+    private OpenShipmentRecordV2Api openShipmentRecordV2Api(Config config) {
+        return null;
+    }
+
+    private OpenShipmentRecordApi openShipmentRecordApi(Config config) {
+        return null;
+    }
+
+    private OpenShipmentNoteV2Api openShipmentNoteV2Api(Config config) {
+        return null;
+    }
+
+    private OpenShipmentNoteApi openShipmentNoteApi(Config config) {
+        return null;
+    }
+
+    private OpenSalesOrderV2Api openSalesOrderV2Api(Config config) {
+        return null;
+    }
+
+    private OpenSalesOrderReturnV2Api openSalesOrderReturnV2Api(Config config) {
+        return null;
+    }
+
+    private OpenSalesOrderReturnApi openSalesOrderReturnApi(Config config) {
+        return null;
+    }
+
+    private OpenSalesOrderApi openSalesOrderApi(Config config) {
+        return null;
+    }
+
+    private OpenReceivingRecordApi openReceivingRecordApi(Config config) {
+        return null;
+    }
+
+    private OpenReceiveNoteV2Api openReceiveNoteV2Api(Config config) {
+        return null;
+    }
+
+    private OpenReceiveNoteApi openReceiveNoteApi(Config config) {
+        return null;
+    }
+
+    private OpenPurchaseReturnOrderV2Api openPurchaseReturnOrderV2Api(Config config) {
+        return null;
+    }
+
+    private OpenPurchaseReturnOrderApi openPurchaseReturnOrderApi(Config config) {
+        return null;
+    }
+
+    private OpenPurchaseRequisitionsV2Api openPurchaseRequisitionsV2Api(Config config) {
+        return null;
+    }
+
+    private OpenPurchaseRequisitionsApi openPurchaseRequisitionsApi(Config config) {
+        return null;
+    }
+
+    private OpenPurchasePlanApi openPurchasePlanApi(Config config) {
+        return null;
+    }
+
+    private OpenPurchaseOrderV2Api openPurchaseOrderV2Api(Config config) {
+        return null;
+    }
+
+    private OpenPurchaseOrderApi openPurchaseOrderApi(Config config) {
+        return null;
+    }
+
+    private OpenPlanFeedbackApi openPlanFeedbackApi(Config config) {
+        return null;
+    }
+
+    private OpenOutsourceSendMaterialV2APi openOutsourceSendMaterialV2APi(Config config) {
+        return null;
+    }
+
+    private OpenOutsourceSendMaterialApi openOutsourceSendMaterialApi(Config config) {
+        return null;
+    }
+
+    private OpenOrderChangeNoteV2Api openOrderChangeNoteV2Api(Config config) {
+        return null;
+    }
+
+    private OpenOrderChangeNoteApi openOrderChangeNoteApi(Config config) {
+        return null;
+    }
+
+    private OpenDeliveryScheduleV2Api openDeliveryScheduleV2Api(Config config) {
+        return null;
+    }
+
+    private OpenDeliveryScheduleNoteV2Api openDeliveryScheduleNoteV2Api(Config config) {
+        return null;
+    }
+
+    private OpenDeliveryScheduleNoteApi openDeliveryScheduleNoteApi(Config config) {
+        return null;
+    }
+
+    private OpenDeliveryScheduleApi openDeliveryScheduleApi(Config config) {
+        return null;
+    }
+
+    private OpenDeliveryAppointmentV2Api openDeliveryAppointmentV2Api(Config config) {
+        return null;
+    }
+
+    private OpenDeliveryAppointmentApi openDeliveryAppointmentApi(Config config) {
+        return null;
+    }
+
+    private OpenCustomerV2Api openCustomerV2Api(Config config) {
+        return null;
     }
 
     @Bean
